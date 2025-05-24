258c2449ea87359b07d4be3f4141f77ff848d704
François Kinet <franois.kinet@gmail.com>
2025-05-24 19:02:21 +0200

    circle placement
    Need to make them unique and part of the Guess struct

--- src/main.rs
+++ src/main.rs
@@ -108,23 +108,56 @@ pub fn camera_zoom_system(
 
 
 
-fn click_in_window(
+// fn click_in_window(
+//     mut mouse_button_input_events: EventReader<MouseButtonInput>,
+//     windows: Query<&Window>,
+// ) {
+//     let window = windows.get_single().unwrap(); // assumes one window
+
+//     for event in mouse_button_input_events.read() {
+//         if event.button == MouseButton::Left && event.state.is_pressed() {
+//             if let Some(position) = window.cursor_position() {
+//                 println!("Clicked at position: {:?}", position);
+
+//             } else {
+//                 println!("Clicked, but cursor was outside of the window.");
+//             }
+//         }
+//     }
+// }
+
+fn click_to_spawn_circle(
+    mut commands: Commands,
     mut mouse_button_input_events: EventReader<MouseButtonInput>,
     windows: Query<&Window>,
+    camera_q: Query<(&Camera, &GlobalTransform)>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let window = windows.get_single().unwrap(); // assumes one window
+    let window = windows.get_single().unwrap();
 
+    let circle = Mesh2d(meshes.add(Circle::new(2.5)));
     for event in mouse_button_input_events.read() {
         if event.button == MouseButton::Left && event.state.is_pressed() {
-            if let Some(position) = window.cursor_position() {
-                println!("Clicked at position: {:?}", position);
-
-            } else {
-                println!("Clicked, but cursor was outside of the window.");
+            if let Some(screen_pos) = window.cursor_position() {
+                // Convert screen pos to world coordinates
+                let Ok((camera, camera_transform)) = camera_q.single() else { todo!() };
+                if let Ok(world_pos) = camera.viewport_to_world_2d(camera_transform, screen_pos) {
+                    // Spawn a tiny white circle at the world position
+                    commands.spawn((
+                        circle.clone(),
+                        MeshMaterial2d(materials.add(Color::WHITE)),
+                        Transform::from_translation(world_pos.extend(0.0)),
+                    ));
+                }
+                else {
+                    todo!()
+                };
             }
         }
     }
 }
+
 
 pub struct HelloPlugin;
 
@@ -133,7 +166,7 @@ impl Plugin for HelloPlugin {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Once)));
         app.insert_resource(LocTimer(Timer::from_seconds(1.0, TimerMode::Once)));
         app.add_systems(Startup, add_city);
-        app.add_systems(Update, (greet_city, find_city, click_in_window));
+        app.add_systems(Update, (greet_city, find_city, click_to_spawn_circle));
     }
 }
 
