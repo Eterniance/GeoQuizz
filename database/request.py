1adb4a33833f37364cd9f1c0a2c0d75470fadd77
François Kinet <franois.kinet@gmail.com>
2025-10-17 14:44:21 +0200

    parsing db in rust

--- database/request.py
+++ database/request.py
@@ -1,22 +1,50 @@
 import requests
 import json
+from pathlib import Path
 
-url = "https://overpass-api.de/api/interpreter"
-headers = {"User-Agent": "personal_geoquizz_game"}
+def filter_json(data):
+    filtered = []
+    for el in data.get("elements", []):
+        tags = el.get("tags", {})
+        name_fr = tags.get("name:fr")
+        name_nl = tags.get("name:nl")
+        name_default = tags.get("name")
 
-query = """
-[out:json][timeout:180];
-area["ISO3166-1"="BE"][admin_level=2]->.country;
-node["place"~"city|town"](area.country);
-out;
-"""
+        filtered.append({
+            "name:default": name_default,
+            "name:fr": name_fr,
+            "name:nl": name_nl,
+            "lat": el["lat"],
+            "lon": el["lon"]
+        })
+    
+    return filtered
 
-response = requests.post(url, data={"data": query}, headers=headers)
-response.raise_for_status()
-data = response.json()
+def main():
+    url = "https://overpass-api.de/api/interpreter"
+    headers = {"User-Agent": "personal_geoquizz_game"}
 
+    query = """
+    [out:json][timeout:180];
+    area["ISO3166-1"="BE"][admin_level=2]->.country;
+    node["place"~"city|town"](area.country);
+    out;
+    """
 
-print(f"Found {len(data.get('elements', []))} city nodes in Belgium")
+    response = requests.post(url, data={"data": query}, headers=headers)
+    response.raise_for_status()
+    data = response.json()
 
-with open("database/belgium_cities.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+    print(f"Found {len(data.get('elements', []))} city nodes in Belgium")
+
+    filtered = filter_json(data)
+
+    print(f"Elements remaining after filtering: {len(filtered)}")
+
+    filepath = script_dir = Path(__file__).parent.resolve() / "belgium_cities.json"
+    
+    with open(filepath, "w", encoding="utf-8") as f:
+        json.dump(filtered, f, ensure_ascii=False, indent=2)
+
+if __name__ == "__main__":
+    main()
